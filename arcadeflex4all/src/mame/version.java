5c40053a6c01ae06abc054fcceaae248a263594b
chusogar <chusogar@gmail.com>
2020-10-06 11:57:44 +0200

    version 0.225

--- arcadeflex4all/src/mame/version.java
+++ arcadeflex4all/src/mame/version.java
@@ -4,7 +4,7 @@ public class version {
     
     public static class version_global
     {
-        static String BARE_BUILD_VERSION = "0.207";
+        static String BARE_BUILD_VERSION = "0.225";
 
         public static String bare_build_version = BARE_BUILD_VERSION;
         public static String build_version = BARE_BUILD_VERSION + " (unknown)";
